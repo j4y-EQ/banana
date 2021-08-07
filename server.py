2beff84b58138ef0f9d02ee7318cf1d75f7d9390
j4y <enqiyue@gmail.com>
2021-08-07 18:49:57 +0800

    my gift website

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+print(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template("index.html")
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+# @app.route('/favicon.ico')
+# def about():
+#     return render_template("about.html")
